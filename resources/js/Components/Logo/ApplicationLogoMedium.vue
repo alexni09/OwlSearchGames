ffbf6c67979abfe233f640c2381ac00a231898ed
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-07-11 17:29:29 -0300

    logo hotfix
    The App Logos' links now route to 'home' instead of '/'.

--- resources/js/Components/Logo/ApplicationLogoMedium.vue
+++ resources/js/Components/Logo/ApplicationLogoMedium.vue
@@ -2,7 +2,7 @@
 import { Link } from '@inertiajs/vue3'
 </script>
 <template>
-    <Link href="/">
+    <Link :href="route('home')">
         <img src="/images/logo/owl96.jpg" class="block h-24 border-2 border-emerald-950 rounded-2xl bg-black opacity-90 shadow-md" />
     </Link>
 </template>
--- resources/js/Components/Logo/ApplicationLogoSmall.vue
+++ resources/js/Components/Logo/ApplicationLogoSmall.vue
@@ -2,7 +2,7 @@
 import { Link } from '@inertiajs/vue3'
 </script>
 <template>
-    <Link href="/">
+    <Link :href="route('home')">
         <img src="/images/logo/owl56.jpg" class="mt-[1px] p-[2px] block h-14 border-2 border-emerald-950 bg-black rounded-2xl opacity-90" />
     </Link>
 </template>
