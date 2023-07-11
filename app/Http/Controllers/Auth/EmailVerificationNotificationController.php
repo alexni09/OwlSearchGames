79785c7940a1bbbf86bd3c8c13524dec4edb011f
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-07-11 19:55:39 -0300

    Email Verification issue hotfix
    Added the try/catch solution to EmailVerificationNotificationController, to fix the Transport Exception issue.

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -7,14 +7,21 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Events\UserRequestedAnotherVerificationEmail;
+use Inertia\Inertia;
+use Inertia\Response;
+use Symfony\Component\Mailer\Exception\TransportException;
 
 class EmailVerificationNotificationController extends Controller {
     /**
      * Sends another email verification notification.
      */
-    public function store(Request $request): RedirectResponse {
+    public function store(Request $request): RedirectResponse|Response {
         if ($request->user()->hasVerifiedEmail()) return redirect()->intended(RouteServiceProvider::HOME);
-        event(new UserRequestedAnotherVerificationEmail($request->user()));
+        try {
+            event(new UserRequestedAnotherVerificationEmail($request->user()));
+        } catch(TransportException) {
+            return Inertia::render('Auth/EmailNotSent');
+        }
         return back()->with('status', 'verification-link-sent');
     }
 }
