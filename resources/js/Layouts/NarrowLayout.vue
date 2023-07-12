21cb769741286572dfdc0126a13098f706a2b045
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-07-12 16:24:40 -0300

    NarrowLayout
    fixed minor css bug

--- resources/js/Layouts/NarrowLayout.vue
+++ resources/js/Layouts/NarrowLayout.vue
@@ -21,7 +21,7 @@ onMounted(() => {
         <div>
             <ApplicationLogo />
         </div>
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-orange-100/80 dark:bg-green-800/80 shadow-md overflow-hidden sm:rounded-lg">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-orange-100 dark:bg-green-800 shadow-md overflow-hidden sm:rounded-lg">
             <slot />
         </div>
     </div>
