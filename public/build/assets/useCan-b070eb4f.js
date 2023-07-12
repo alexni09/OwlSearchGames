cab3ead63d8055c15a456a8cd417cfe317473bc9
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-07-12 16:05:06 -0300

    EmailNotes
    More changes on the EmailNotes.vue text messages.

--- public/build/assets/useCan-b070eb4f.js
+++
@@ -1,1 +0,0 @@
-import{t as r,K as t}from"./app-f83a01d3.js";function e(){function s(o){const n=r(t().props.auth.permissions);return n==null?!1:n.includes(o)}return{can:s}}export{e as u};
--- resources/js/Components/Email/EmailNotes.vue
+++ resources/js/Components/Email/EmailNotes.vue
@@ -1,8 +1,18 @@
 <template>
     <div class="emailNotesStyle">{{ $msg.emailVerificationMessageBelowNotes }}</div>
     <div class="emailNotesStyle">• {{ $msg.emailVerificationMessageBelowSpam }}</div>
-    <div class="emailNotesStyle">• {{ $msg.emailVerificationMessageBelowDomains1 }} <span class="domainStyle">owlsearchgames.com</span> {{ $msg.emailVerificationMessageBelowDomains2 }}</div>
-    <div class="emailNotesStyle">• {{ $msg.emailVerificationMessageBelowNoReply1 }} <span class="domainStyle">noreply@owlsearchgames.com</span> {{ $msg.emailVerificationMessageBelowNoReply2 }}</div>
+    <div class="emailNotesStyle">• {{ $msg.emailVerificationMessageBelowDomains1 }} 
+                                    <span class="domainStyle">owlsearchgames.com</span> 
+                                    {{ $msg.emailVerificationMessageBelowDomains2 }}
+                                    <span class="underline">{{ $msg.emailVerificationMessageBelowDomains3 }}</span>
+                                    {{ $msg.emailVerificationMessageBelowDomains4 }}
+    </div>
+    <div class="emailNotesStyle">• {{ $msg.emailVerificationMessageBelowNoReply1 }} 
+                                    <span class="domainStyle">noreply@owlsearchgames.com</span> 
+                                    {{ $msg.emailVerificationMessageBelowNoReply2 }}
+                                    <span class="underline">{{ $msg.emailVerificationMessageBelowNoReply3 }}</span>
+                                    {{ $msg.emailVerificationMessageBelowNoReply4 }}
+    </div>
     <div class="h-1" />
 </template>
 <style lang="postcss" scoped>
