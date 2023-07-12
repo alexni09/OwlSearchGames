3bed0a2b7be7be8a48f7b74bae066e807da71e26
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-07-12 10:59:39 -0300

    EmailNotes
    updated the text messages

---
+++ public/build/assets/MustUpdateLocale-a76b28ea.js
@@ -0,0 +1,1 @@
+import{K as t,x as o}from"./app-f83a01d3.js";const l={__name:"MustUpdateLocale",setup(p){const e=o(),a=t().props.auth.locale;return t().props.mustUpdateLocale&&e.appContext.config.globalProperties.$setLocale(a),(c,n)=>null}};export{l as _};
--- resources/js/Components/Email/EmailNotes.vue
+++ resources/js/Components/Email/EmailNotes.vue
@@ -1,15 +1,14 @@
 <template>
     <div class="emailNotesStyle">{{ $msg.emailVerificationMessageBelowNotes }}</div>
     <div class="emailNotesStyle">• {{ $msg.emailVerificationMessageBelowSpam }}</div>
-    <div class="emailNotesStyle">• {{ $msg.emailVerificationMessageBelowDomains }}</div>
-    <ul class="">
-        <li class="ml-5 emailNotesStyle">• owlsearchgame.com</li>
-        <li class="ml-5 emailNotesStyle">• owlsearchgames.com</li>
-    </ul>
-    <div class="emailNotesStyle">• {{ $msg.emailVerificationMessageBelowNoReply }}</div>
+    <div class="emailNotesStyle">• {{ $msg.emailVerificationMessageBelowDomains1 }} <span class="domainStyle">owlsearchgames.com</span> {{ $msg.emailVerificationMessageBelowDomains2 }}</div>
+    <div class="emailNotesStyle">• {{ $msg.emailVerificationMessageBelowNoReply1 }} <span class="domainStyle">noreply@owlsearchgames.com</span> {{ $msg.emailVerificationMessageBelowNoReply2 }}</div>
     <div class="h-1" />
 </template>
 <style lang="postcss" scoped>
+.domainStyle {
+    @apply underline font-bold;
+}
 .emailNotesStyle {
     @apply font-semibold text-sm text-orange-700 dark:text-amber-100;
 }
