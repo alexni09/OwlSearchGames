addf539900287d1ecba4fb671aab6660d597fe65
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-07-14 12:20:39 -0300

    locale 10
    Added LocaleCycler to SendRedefinePasswordEmail

--- public/build/assets/useChangeLocaleAxios-ee23d688.js
+++
@@ -1,1 +0,0 @@
-function c(){function n(e){axios.post("/api/locale",{locale:e}).then(function(o){}).catch(function(o){console.log(o)})}return{changeLocaleAxios:n}}export{c as u};
--- resources/js/Pages/Auth/SendRedefinePasswordEmail.vue
+++ resources/js/Pages/Auth/SendRedefinePasswordEmail.vue
@@ -1,13 +1,17 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3'
 import NarrowLayout from '@/Layouts/NarrowLayout.vue'
+import LocaleCycler from '@/Components/Others/LocaleCycler.vue'
 import HomeSVG from '@/Components/SVG/HomeSVG.vue'
 import EmailNotes from '@/Components/Email/EmailNotes.vue'
 </script>
 <template>
     <Head :title="$msg.redefinePassword" />
     <NarrowLayout>
-        <h3 class="mt-1 mb-2 text-lg text-orange-800 dark:text-amber-100 font-semibold">{{ $msg.redefinePassword }}</h3>
+        <div class="flex justify-between items-start">
+            <h3 class="mt-1 mb-2 pr-1 text-lg text-orange-800 dark:text-amber-100 font-semibold">{{ $msg.redefinePassword }}</h3>
+            <LocaleCycler class="mt-1" />
+        </div>
         <p class="mt-1 mb-4 text-orange-700 dark:text-amber-100">{{ $msg.redefinePasswordText }}</p>
         <EmailNotes />
         <div class="flex flex-row-reverse">
