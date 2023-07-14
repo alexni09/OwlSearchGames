ef4f2d19c3f7c58310c7250b0e307e21347fd49e
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-07-14 10:54:04 -0300

    Locale 7
    Added LocaleCycler to EmailNotSent

--- resources/js/Pages/Auth/EmailNotSent.vue
+++ resources/js/Pages/Auth/EmailNotSent.vue
@@ -1,11 +1,15 @@
 <script setup>
 import { Link } from '@inertiajs/vue3'
 import NarrowLayout from '@/Layouts/NarrowLayout.vue'
+import LocaleCycler from '@/Components/Others/LocaleCycler.vue'
 import HomeSVG from '@/Components/SVG/HomeSVG.vue'
 </script>
 <template>
     <NarrowLayout>
-        <h3 class="mt-1 mb-2 text-lg text-orange-800 dark:text-amber-100 font-semibold">{{ $msg.whoops }}</h3>
+        <div class="flex justify-between items-start">
+            <h3 class="mt-1 mb-2 text-lg text-orange-800 dark:text-amber-100 font-semibold">{{ $msg.whoops }}</h3>
+            <LocaleCycler class="mt-1" />
+        </div>
         <p class="mt-1 mb-4 text-orange-700 dark:text-amber-100">{{ $msg.somethingWentWrong }}</p>
         <p class="mt-1 mb-4 text-orange-700 dark:text-amber-100">{{ $msg.tryAgainLater }}</p>
         <div class="flex flex-row-reverse">
