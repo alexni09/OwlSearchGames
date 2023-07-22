19830497924cd51fc8886e99619272d338ddc796
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-07-22 17:23:36 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Api\CaptchaReloadController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('captchareload', [CaptchaReloadController::class, 'show'])->name('captcha.reload');
