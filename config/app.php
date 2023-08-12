e9834f778ccf676b95e58a3385db1e18d0d90486
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-08-12 09:08:09 -0300

    Redis test

--- config/app.php
+++ config/app.php
@@ -182,7 +182,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'Example' => App\Facades\Example::class,
+        'Redis' => Illuminate\Support\Facades\Redis::class
     ])->toArray(),
 
 ];
