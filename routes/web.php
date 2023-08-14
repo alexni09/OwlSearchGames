1e21c276959f6b5e8a6cc4107f79e1c9557b9147
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-08-14 19:50:41 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -91,7 +91,7 @@ Route::middleware('auth')->group(function () {
     Route::get('verify-email', EmailVerificationPromptController::class)
                 ->name('verification.notice');
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-                ->middleware(['signed', 'throttle:6,1'])
+                ->middleware(['throttle:6,1'])
                 ->name('verification.verify');
     Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
                 ->middleware('throttle:6,1')
