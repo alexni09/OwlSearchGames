acdcec51f2fa4a5514b9cc32c8da591d2ae4a3e7
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-08-16 07:57:16 -0300

    UserRequestedAnotherVerificationEmail
    Event removed.

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use App\Events\UserRequestedAnotherVerificationEmail;
 use Inertia\Response;
 use App\Jobs\SendEmailVerification;
 
@@ -16,7 +15,6 @@ class EmailVerificationNotificationController extends Controller {
      */
     public function store(Request $request): RedirectResponse|Response {
         if ($request->user()->hasVerifiedEmail()) return redirect()->intended(RouteServiceProvider::HOME);
-        event(new UserRequestedAnotherVerificationEmail($request->user()));
         SendEmailVerification::dispatch($request->user());
         return back()->with('status', 'verification-link-sent');
     }
